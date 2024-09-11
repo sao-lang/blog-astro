2e98cf72a61fce1cfed128cb7d3dcc72d7ab0c2c
sao-lang <3089549839@qq.com>
2024-09-11 23:14:07 +0800

    feat: update player

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,31 @@
 import { defineConfig } from 'astro/config';
-
 import AutoImport from 'unplugin-auto-import/vite';
 import vue from '@astrojs/vue';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [vue()],
-    redirects: {
-        '/': '/home',
+  integrations: [vue(), tailwind()],
+  redirects: {
+    '/': '/home'
+  },
+  devToolbar: {
+    enabled: false
+  },
+  vite: {
+    plugins: [AutoImport({
+      imports: ['vue'],
+      // 自动导入 Vue 的 Composition API
+      dts: 'src/auto-imports.d.ts' // 生成自动导入的类型声明文件
+    }), tsconfigPaths() // 自动解析 tsconfig 中的路径别名
+    ],
+    resolve: {
+      extensions: ['.js', '.ts', '.vue'] // 加入 .vue 后缀
     },
-    devToolbar: {
-        enabled: false,
-    },
-    vite: {
-        plugins: [
-            AutoImport({
-                imports: ['vue'], // 自动导入 Vue 的 Composition API
-                dts: 'src/auto-imports.d.ts', // 生成自动导入的类型声明文件
-            }),
-            tsconfigPaths(), // 自动解析 tsconfig 中的路径别名
-        ],
-        resolve: {
-            extensions: ['.js', '.ts', '.vue'], // 加入 .vue 后缀
-        },
-        alias: {
-            '@': '/src', // 定义 @styles 别名指向 src/styles 目录
-        },
-    },
+    alias: {
+      '@': '/src' // 定义 @styles 别名指向 src/styles 目录
+    }
+  }
 });
--- src/components/player-new/index.vue
+++ src/components/player-new/index.vue
@@ -1,16 +1,60 @@
 <template>
-    <div class="player"></div>
+    <div class="fixed left-0 bottom-0 w-66 h-66 player">
+        <img
+            :src="current.singerPhoto"
+            class="relative w-full h-full object-cover player__singer-photo"
+        />
+        <div
+            class="absolute top-0 bg-white player__toolbar-container transition-all duration-500 ease-in-out"
+            :style="{ left: `${toolbarLeft}px` }"
+        >
+            <div class="flex justify-between items-center w-full player__toolbar-top">
+                <span class="truncate leading-none player__song-info">
+                    {{ title }}
+                </span>
+                <div class="flex player__song-info"></div>
+            </div>
+            <div
+                class="flex justify-center items-center absolute top-0 right-0 h-full cursor-pointer player__toolbar-toggle"
+                @click="isToolbarOpen = !isToolbarOpen"
+            />
+        </div>
+    </div>
 </template>
 
 <script setup lang="ts">
     import { songs } from '@/mock/songs';
+    const currentIndex = ref(0);
+    const current = computed(() => songs[currentIndex.value]);
+    const isToolbarOpen = ref(false);
+    const toolbarLeft = computed(() => (isToolbarOpen.value ? 66 : -268));
+    const title = computed(() => `${current.value.singer}-${current.value.name}`);
 </script>
 
 <style lang="scss">
     @import '@lania/utils-scss/mixin.scss';
     .player {
-        @include fixed($left: 0, $bottom: 0);
-        @include box-model($width: 66px, $height: 66px);
-        @include background($color: var(--bg-color));
+        background-color: var(--bg-color);
+        width: 66px;
+        height: 66px;
+        .player__singer-photo {
+            z-index: 2;
+        }
+        .player__toolbar-container {
+            left: 66px;
+            width: 346px;
+            height: 66px;
+            padding: 14px 19px 0 10px;
+            .player__toolbar-top {
+                .player__song-info {
+                    max-width: 212px;
+                    font-size: 14px;
+                }
+            }
+            .player__toolbar-toggle {
+                width: 12px;
+                background-color: #e6e6e6;
+            }
+        }
     }
 </style>
