e3b01ee7fad26b89811971f70a14daae60251d09
sao-lang <3089549839@qq.com>
2024-09-24 23:24:30 +0800

    feat: update player

--- src/components/progress/index.vue
+++ src/components/progress/index.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-    import { computed, onBeforeUnmount, ref, type PropType, nextTick } from 'vue';
+    import type { CSSProperties } from 'vue';
 
     const props = defineProps({
         percent: {
@@ -9,6 +9,18 @@
         direction: {
             type: String as PropType<'horizontal' | 'vertical'>,
             default: 'horizontal',
+        },
+        barStyle: {
+            type: Object as PropType<CSSProperties>,
+            default: () => ({}),
+        },
+        dotStyle: {
+            type: Object as PropType<CSSProperties>,
+            default: () => ({}),
+        },
+        progressStyle: {
+            type: Object as PropType<CSSProperties>,
+            default: () => ({}),
         },
     });
     const emits = defineEmits(['update:percent', 'mousedown', 'mouseup', 'mousemove', 'change']);
@@ -25,19 +37,52 @@
         },
     });
     const isMouseDown = ref(false);
+    const isHorizontal = computed(() => props.direction === 'horizontal');
+    const computedProgressStyle = computed(() => {
+        if (isHorizontal.value) {
+            return { width: `${percent.value * 100}%` };
+        }
+        return {
+            height: `${percent.value * 100}%`,
+        };
+    });
+    const computedBarStyle = computed(() => {
+        if (isHorizontal.value) {
+            return { left: `${percent.value * 100}%` };
+        }
+        return {
+            bottom: `${percent.value * 100}%`,
+        };
+    });
 
     onBeforeUnmount(() => {
         document.removeEventListener('mouseup', handleDocumentMouseUp);
     });
 
     const handleProcessWidthChange = (e: MouseEvent) => {
-        const [{ width, left }] = bar.value?.getClientRects()!;
-        const x = e.pageX - left;
-        const maxX = width - dot.value!.offsetWidth;
-        const offsetX = x < 0 ? 0 : x > maxX ? maxX : x;
-        const value = Number((offsetX / maxX).toFixed(4));
+        const { offset, maxDistance } = getDiffDistance(e);
+        const value = Number((offset / maxDistance).toFixed(4));
         percent.value = value;
         emits('change', value);
+    };
+    const getDiffDistance = (e: MouseEvent) => {
+        const [{ width, left, height, bottom }] = bar.value?.getClientRects()!;
+        if (isHorizontal.value) {
+            const x = e.pageX - left;
+            const maxDistance = width - dot.value!.offsetWidth;
+            const offset = x < 0 ? 0 : x > maxDistance ? maxDistance : x;
+            return {
+                offset,
+                maxDistance,
+            };
+        }
+        const y = bottom - e.pageY;
+        const maxDistance = height - dot.value!.offsetHeight;
+        const offset = y < 0 ? 0 : y > maxDistance ? maxDistance : y;
+        return {
+            offset,
+            maxDistance,
+        };
     };
     const handleDotMouseDown = (e: MouseEvent) => {
         e.preventDefault();
@@ -75,22 +120,25 @@
     <div
         class="bar"
         :class="{ 'bar--vertical': props.direction === 'vertical' }"
+        :style="props.barStyle"
         ref="bar"
         @mousedown="handleMouseDown"
         @mouseup="handleBarMouseUp"
     >
-        <div class="progress-wrapper">
+        <div class="bar__progress--wrapper">
             <div
-                :class="['progress', { 'add-transition': !isMouseDown }]"
-                :style="{ width: `${percent * 100}%` }"
+                :class="['bar__progress', { 'add-transition': !isMouseDown }]"
+                :style="{ ...props.progressStyle, ...computedProgressStyle }"
             />
         </div>
         <div
             ref="dot"
-            :style="{ left: `${percent * 100}%` }"
-            :class="['dot', { 'add-transition': !isMouseDown, 'add-hover': isMouseDown }]"
+            :style="{ ...props.dotStyle, ...computedBarStyle }"
+            :class="['bar__dot', { 'add-transition': !isMouseDown, 'add-hover': isMouseDown }]"
             @mousedown="handleDotMouseDown"
-        />
+        >
+            <div class="bar__dot--inner"></div>
+        </div>
     </div>
 </template>
 <style lang="scss" scoped>
